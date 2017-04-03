130ba4b0f6dfd70204b115111049392f64305d78
sensorsiot <andreas.spiess@arumba.com>
2017-04-03 20:54:04 +0200

    Small changes

--- run-fan.py
+++ run-fan.py
@@ -15,7 +15,7 @@ def setup():
 def getCPUtemperature():
     res = os.popen('vcgencmd measure_temp').readline()
     temp =(res.replace("temp=","").replace("'C\n",""))
-    #print("temp is {0}".format(temp)) #Uncomment here for testing
+    print("temp is {0}".format(temp)) #Uncomment here for testing
     return temp
 def fanON():
     setPin(True)
